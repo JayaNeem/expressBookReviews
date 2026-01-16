f7571c27ecb835bfa5a603101e76c1de01cdc9a5
JayaNeem <jaya.neem@gmail.com>
2026-01-16 10:27:19 -0500

    Improved performance by adding asychronous behaviour to general users endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,35 +21,69 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    let promise = new Promise((resolve, reject) => {
+        if (books) 
+            resolve(JSON.stringify(books,null,4));
+        else
+            reject("No Books found");
+    });
+    promise.then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.status(404).send(error);
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    let promise = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        if (books[isbn]) 
+            resolve(books[isbn]);
+        else
+            reject("No book exist with isbn ", isbn);
+    });
+    promise.then((result)=>{
+        res.send(result);
+    }).catch((error) => {
+        res.status(404).send(error);
+    });    
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  for (const isbn of Object.keys(books)) {
-    if(books[isbn].author == author) {
-        return res.send(books[isbn]);
+  let promise = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    for (const isbn of Object.keys(books)) {
+      if(books[isbn].author == author) {
+        return resolve(books[isbn]);
+      }
     }
-  }
-  return res.status(400).json({message: "Invalid Author"});
+    reject("Invalid Author");
+  });
+  promise.then((result) => {
+    res.send(result);
+  }).catch((error) => {
+    res.status(400).send(error);
+  });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
+  let promise = new Promise((resolve, reject) => {
+  const title = req.params.title;
   for (const isbn of Object.keys(books)) {
     if(books[isbn].title == title) {
-        return res.send(books[isbn]);
+        return resolve(books[isbn]);
+      }
     }
-  }
-  return res.status(400).json({message: "Invalid Title"});
+    reject("Invalid Title");
+  });
+  promise.then((result) => {
+    res.send(result);
+  }).catch((error) => {
+    res.status(400).send(error);
+  });
 });
 
 //  Get book review
